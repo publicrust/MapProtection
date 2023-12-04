bbec23e969a8dd19c16acaa5df91e1741b590747
Skuli Dropek <152790416+freerust12@users.noreply.github.com>
2023-12-04 22:03:18 +0300

    Добавил DMCAProtection

---
+++ DMCAProtection/Facepanch/RustWorldSDK/WorldSerialization.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using ProtoBuf;
+using LZ4;
+using System.Windows;
+using UnityEngine;
+
+public class WorldSerialization
+{
+    public const uint CurrentVersion = 9;
+
+    public uint Version { get; set; }
+
+    public WorldData world = new WorldData();
+
+    public WorldSerialization()
+    {
+        Version = CurrentVersion;
+    }
+
+    [ProtoContract]
+    public class WorldData
+    {
+        [ProtoMember(1)] public uint size = 4000;
+        [ProtoMember(2)] public List<MapData> maps = new List<MapData>();
+        [ProtoMember(3)] public List<PrefabData> prefabs = new List<PrefabData>();
+        [ProtoMember(4)] public List<PathData> paths = new List<PathData>();
+    }
+
+    [ProtoContract]
+    public class MapData
+    {
+        [ProtoMember(1)] public string name;
+        [ProtoMember(2)] public byte[] data;
+    }
+
+    [Serializable]
+    [ProtoContract]
+    public class PrefabData
+    {
+        [ProtoMember(1)] public string category;
+        [ProtoMember(2)] public uint id;
+        [ProtoMember(3)] public VectorData position;
+        [ProtoMember(4)] public VectorData rotation;
+        [ProtoMember(5)] public VectorData scale;
+
+
+        public PrefabData() { }
+        public PrefabData(string category, uint id, VectorData position, VectorData rotation, VectorData scale)
+        {
+            this.category = category;
+            this.id = id;
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+    }
+
+    [Serializable]
+    [ProtoContract]
+    public class PathData
+    {
+        [ProtoMember(1)] public string name;
+        [ProtoMember(2)] public bool spline;
+        [ProtoMember(3)] public bool start;
+        [ProtoMember(4)] public bool end;
+        [ProtoMember(5)] public float width;
+        [ProtoMember(6)] public float innerPadding;
+        [ProtoMember(7)] public float outerPadding;
+        [ProtoMember(8)] public float innerFade;
+        [ProtoMember(9)] public float outerFade;
+        [ProtoMember(10)] public float randomScale;
+        [ProtoMember(11)] public float meshOffset;
+        [ProtoMember(12)] public float terrainOffset;
+        [ProtoMember(13)] public int splat;
+        [ProtoMember(14)] public int topology;
+        [ProtoMember(15)] public VectorData[] nodes;
+    }
+
+    [Serializable]
+    [ProtoContract]
+    public class VectorData
+    {
+        [ProtoMember(1)] public float x;
+        [ProtoMember(2)] public float y;
+        [ProtoMember(3)] public float z;
+
+        public VectorData()
+        {
+        }
+
+        public VectorData(float x, float y, float z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+
+        public static implicit operator VectorData(Vector3 v)
+        {
+            return new VectorData(v.x, v.y, v.z);
+        }
+
+        public static implicit operator VectorData(Quaternion q)
+        {
+            return q.eulerAngles;
+        }
+
+        public static implicit operator Vector3(VectorData v)
+        {
+            return new Vector3(v.x, v.y, v.z);
+        }
+
+        public static implicit operator Quaternion(VectorData v)
+        {
+            return Quaternion.Euler(v);
+        }
+
+    }
+
+    public MapData GetMap(string name)
+    {
+        for (int i = 0; i < world.maps.Count; i++)
+            if (world.maps[i].name == name) return world.maps[i];
+        return null;
+    }
+
+    public void AddMap(string name, byte[] data)
+    {
+        var map = new MapData();
+
+        map.name = name;
+        map.data = data;
+
+        world.maps.Add(map);
+    }
+
+    public void Save(string fileName)
+    {
+        try
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                using (var binaryWriter = new BinaryWriter(fileStream))
+                {
+                    binaryWriter.Write(Version);
+
+                    using (var compressionStream = new LZ4Stream(fileStream, LZ4StreamMode.Compress))
+                        Serializer.Serialize(compressionStream, world);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+
+    public void Load(string fileName)
+    {
+        try
+        {
+            using (var fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (var binaryReader = new BinaryReader(fileStream))
+                {
+                    Version = binaryReader.ReadUInt32();
+                   // if (Version != CurrentVersion)
+                   //   MessageBox.Show("Map Version is: " + Version + " whilst Rust is on: " + CurrentVersion);
+
+                    using (var compressionStream = new LZ4Stream(fileStream, LZ4StreamMode.Decompress))
+                        world = Serializer.Deserialize<WorldData>(compressionStream);
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            MessageBox.Show(e.Message);
+        }
+    }
+}
---
+++ DMCAProtection/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Ookii.Dialogs.Wpf;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DMCAProtection
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var path = GetPathOpenFileDialog();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Path null");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(TextBox.Text))
+            {
+                MessageBox.Show("DMCA Protection has null");
+                return;
+            }
+
+            var worldSerialization = new WorldSerialization();
+
+            worldSerialization.Load(path);
+
+            worldSerialization.AddMap("DMCA", Encoding.UTF8.GetBytes(TextBox.Text));
+            worldSerialization.Save(path + ".DMCA.map");
+        }
+
+        private string GetPathOpenFileDialog()
+        {
+            VistaOpenFileDialog openFileDialog = new VistaOpenFileDialog();
+            openFileDialog.Filter = "Карты раст (*.map)|*.map";
+
+            if (openFileDialog.ShowDialog() == false)
+            {
+                return "";
+            }
+
+            return openFileDialog.FileName;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            var path = GetPathOpenFileDialog();
+
+            if (string.IsNullOrEmpty(path))
+            {
+                MessageBox.Show("Path null");
+                return;
+            }
+
+            var worldSerialization = new WorldSerialization();
+
+            worldSerialization.Load(path);
+
+            TextBlock.Text = Encoding.UTF8.GetString(worldSerialization.GetMap("DMCA").data);
+        }
+    }
+}
