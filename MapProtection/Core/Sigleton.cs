39c85fab65d8c0f5af44a7f4e183a0411b9f072c
freerust <152790416+freerust12@users.noreply.github.com>
2023-12-23 00:16:35 +0400

    Create Sigleton.cs

---
+++ MapProtection/Core/Sigleton.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MapProtection.Core
+{
+    internal abstract class Sigleton<T> where T : Sigleton<T>
+    {
+        public static T Instance;
+
+        public Sigleton()
+        {
+            if (Instance == null)
+            {
+                Instance = (T)this;
+                return;
+            }
+
+            throw new Exception("Dont create new Sigleton");
+        }
+    }
+}
