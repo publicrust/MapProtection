074d56282cc5a5922eacc9fad873cee135436e03
freerust <152790416+freerust12@users.noreply.github.com>
2024-03-02 13:27:47 +0400

    Update RustPlugin.cs

--- Library/Core/RustPlugin.cs
+++ Library/Core/RustPlugin.cs
@@ -27,7 +27,7 @@ using UnityEngine;
 
 namespace Oxide.Plugins
 {
-    [Info(""MapProtection"", ""bmgjet & FREE RUST"", ""1.1.4"")]
+    [Info(""MapProtection"", ""bmgjet & FREE RUST"", ""1.1.5"")]
     [Description(""MapProtection"")]
     class MapProtection : RustPlugin
     {
