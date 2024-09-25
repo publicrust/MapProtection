1f9bb495ee0074fe78fbc6789c1ce8886233e271
freerust <152790416+freerust12@users.noreply.github.com>
2024-09-25 22:18:10 +0400

    Update Program.cs

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -3,21 +3,17 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 
-// Добавляем поддержку CORS
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAllOrigins",
         builder => builder
-            .AllowAnyOrigin() // Разрешить любые источники
-            .AllowAnyMethod() // Разрешить любые методы (GET, POST, PUT и т.д.)
-            .AllowAnyHeader()); // Разрешить любые заголовки
+            .AllowAnyOrigin() 
+            .AllowAnyMethod()
+            .AllowAnyHeader());
 });
 
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
@@ -31,7 +27,6 @@ builder.WebHost.ConfigureKestrel(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -40,7 +35,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-// Включаем middleware CORS
 app.UseCors("AllowAllOrigins");
 
 app.UseAuthorization();
